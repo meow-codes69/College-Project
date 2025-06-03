99331c25197a190c09529bda791225e6436e8c35
Uday Pratap Chauhan <udaypratapc93@gmail.com>
2025-06-03 13:27:18 +0530

    all setups configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 const app = express()
 
@@ -12,7 +13,15 @@ app.use(cors({
 // express.json lets the server use json data
 app.use(express.json({limit:"16kb"}))//giving limit of 16kb json data
 app.use(express.urlencoded({extended:"true", limit:"16kb"}))
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static("public"))
 app.use(cookieParser())
+
+// Routes import
+import userRouter from "./routes/user.routes.js"
+
+// Router declaration
+app.use("/api/v1/users",userRouter)
+
+// http://localhost:3000/api/vi/users/register
 
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,16 +1,18 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+export { asyncHandler }
+
 // const asyncHandler= (requestHandler)=>(req, res, next)=>{
 //         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next (err))
 //     }
 
 
 // writing this code so that we don't need to use trycatch again and again
-const asyncHandler =(fn) => async (req, res, next )=>{
-    try {
-        await fn (req, res, next)
-    } catch (error) {
-        res.status(500).json({
-            success:false,
-            message:error.message
-        })
-    }
-}
